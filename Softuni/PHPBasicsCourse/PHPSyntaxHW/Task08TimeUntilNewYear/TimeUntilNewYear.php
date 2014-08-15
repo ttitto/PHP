00d6ace531182f0389a0b6286f278cf9838a1115
ttitto <ttitto@users.noreply.github.com>
2014-08-15 08:56:08 +0300

    Task 08 solved

--- Softuni/PHPBasicsCourse/PHPSyntaxHW/Task08TimeUntilNewYear/TimeUntilNewYear.php
+++ Softuni/PHPBasicsCourse/PHPSyntaxHW/Task08TimeUntilNewYear/TimeUntilNewYear.php
@@ -1,22 +1,19 @@
 <?php
-$current_date=new DateTime('now', new DateTimeZone('EUROPE/Sofia'));
+$current_date = new DateTime('now', new DateTimeZone('EUROPE/Sofia'));
 
-print_r($current_date);
-$new_year_date=new DateTime();
-$new_year_date->setDate( $current_date->format('Y')+1,1,1);
-$new_year_date->setTime(-1,0,0);
+$new_year_date = new DateTime();
+$new_year_date->setDate(intval($current_date->format('Y')) + 1, 1, 1);
+$new_year_date->setTime(-1, 0, 0);
 $new_year_date->setTimezone(new DateTimeZone('EUROPE/Sofia'));
-print_r($new_year_date);
 
-//$new_year_date=getdate($new_year_date->getTimestamp());
-////$new_year_date->setTimezone(new DateTimeZone('Europe/Sofia'));
-//$diff=date_diff( $new_year_date,$current_date);
-//
-//print_r($diff);
-$timestamp_diff= $new_year_date->getTimestamp()-$current_date->getTimestamp();
+$timestamp_diff = $new_year_date->getTimestamp() - $current_date->getTimestamp();
+$time_diff = getdate($timestamp_diff);
 
-print_r($timestamp_diff);
-printf( "Hours until new year : %.0d\nMinutes until new year : %.0d\n Seconds until new year : %
-.0d\nDays:Hours:Minutes:Seconds '%.0d:%.0d:%.0d:%.0d'",
-    ($timestamp_diff/60/60),
-    $timestamp_diff/60);
+printf("Hours until new year : %.0d\nMinutes until new year : %.0d\nSeconds until new year : %.0d\nDays:Hours:Minutes:Seconds %.0d:%.0d:%.0d:%.0d",
+    $timestamp_diff / 60 / 60,
+    $timestamp_diff / 60,
+    $timestamp_diff,
+    $time_diff['yday'],
+    $time_diff['hours'],
+    $time_diff['minutes'],
+    $time_diff['seconds']);
