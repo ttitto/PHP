c9c3ed08eaed7cd7eeb2b8fc43bcdcedfa960baf
ttitto <ttitto@users.noreply.github.com>
2014-08-24 22:41:50 +0300

    task 7 started

---
+++ Softuni/PHPBasicsCourse/PHPFlowControlHW/Task07StudentSorting/StudentSorting.js
@@ -0,0 +1,3 @@
+/**
+ * Created by ttitto on 14-8-24.
+ */
---
+++ Softuni/PHPBasicsCourse/PHPFlowControlHW/Task07StudentSorting/StudentSorting.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Student Sorting</title>
+</head>
+<body>
+
+<form action="" method="post">
+   <table border="1" >
+       <thead><tr><th>First Name:</th><th>Second Name:</th><th>Email:</th><th>Exam score:</th></tr></thead>
+       <tbody>
+       <tr class="data-row">
+           <td>
+               <input type="text"/>
+               <input type="text"/>
+               <input type="text"/>
+               <input type="number"/>
+               <input type="button" value="-"/>
+           </td>
+       </tr>
+       </tbody>
+   </table>
+
+</form>
+
+
+<?php
+
+
+?>
+
+
+</body>
+</html>
