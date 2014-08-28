c329b58d57814cd4468ebaa54c1dfdea8f0f6db4
ttitto <ttitto@users.noreply.github.com>
2014-08-28 23:50:04 +0300

    Task04 solved

--- Softuni/PHPBasicsCourse/PHPExamPrep/Task04PHPChess/PHPChess.php
+++ Softuni/PHPBasicsCourse/PHPExamPrep/Task04PHPChess/PHPChess.php
@@ -5,34 +5,41 @@
 
 <?php
 $result = '';
-$json_base = array();
+$json_base = array('Bishop' => 0,'Horseman' => 0,'King' => 0,'Pawn' => 0, 'Queen' => 0,'Rook' => 0    );
 $pieces = array('R', 'H', 'B', 'K', 'Q', 'P', ' ');
 $input_rows = explode('/', $_GET['board']);
+$result.="<table>";
 for ($i = 0; $i < count($input_rows); $i++) {
     $board[$i] = explode('-', $input_rows[$i]);
     if (0 != count(array_diff($board[$i], $pieces)) || count($board[$i]) != 8) {
         echo "<h1>Invalid chess board</h1>";
         exit;
     }
-    $result .= "<table><tr>";
+    $result .= "<tr>";
     foreach ($board[$i] as $piece) {
         $result .= "<td>$piece</td>";
-        $board[$i] = array_map('assign_full_key', $board[$i]);
-        $board[$i] = array_count_values($board[$i]);
-        var_dump($board[$i]);
     }
-
-
     $result .= "</tr>";
 
+    $mapped = array_map('assign_full_key', $board[$i]);
+    $mapped = array_count_values($mapped);
+    foreach ($mapped as $key=>$val) {
+        $json_base[$key] = $json_base[$key] + $val;
+    }
 }
-var_dump($input_rows);
-echo($result);
+$result.= "</table>";
+unset ($json_base['']);
+foreach ($json_base as $key => $val) {
+    if ($val==0) {
+       unset($json_base[$key]);
+    }
+}
 
-
+$result.=json_encode($json_base);
+echo "$result";
 function assign_full_key($kv)
 {
-    switch ($kv->key) {
+    switch ($kv) {
         case 'Q':
             return 'Queen';
         case 'R':
